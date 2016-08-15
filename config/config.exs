78731a0205f8fed8a7fa45353d174253a9453714
Stephen M. Pallen <smpallen99@yahoo.com>
2016-08-15 10:20:41 -0400

    initial import

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :store,
+  ecto_repos: [Store.Repo]
+
+# Configures the endpoint
+config :store, Store.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "IWUxOms1AEfjIzzLSHPY5w+8m4FpBAhro1rTlDTzzRpE72HGA4iSZaupl9+q2gIZ",
+  render_errors: [view: Store.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Store.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ test/test_helper.exs
@@ -0,0 +1,4 @@
+ExUnit.start
+
+Ecto.Adapters.SQL.Sandbox.mode(Store.Repo, :manual)
+
